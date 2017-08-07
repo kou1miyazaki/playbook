b523ab1958b60af543bceef679366f0130434c42
Kouichi Miyazaki <kou1miyazaki@gmail.com>
2017-08-08 06:49:09 +0900

    initial commit

---
+++ git-push.sh
@@ -0,0 +1,6 @@
+git rm *.retry
+git config --global user.email "kou1miyazaki@gmail.com"
+git config --global user.name "Kouichi Miyazaki"
+git add *
+git commit -m "initial commit"
+git push origin master
