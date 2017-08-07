6c88166d9cbb6bc2db72191d0aa0e4da35abbeea
Kouichi Miyazaki <kou1miyazaki@gmail.com>
2017-08-08 07:06:40 +0900

    initial commit

--- bin/ansible-centos.sh
+++
@@ -1,5 +0,0 @@
-yum -y install epel-release ansible
-cd
-cp -fr /opt/playbook/hosts /etc/ansible
-ssh-keygen
-ssh-copy-id 127.0.0.1
--- git-clone.sh
+++
@@ -1,5 +0,0 @@
-cd /opt
-rm -fr playbook
-git clone https://kou1miyazaki@github.com/kou1miyazaki/playbook.git
-cd /opt/playbook
-
